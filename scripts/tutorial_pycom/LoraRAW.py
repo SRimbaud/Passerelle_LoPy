2defbb31ade8c6d2a2dbde743325c627888a9b20
Rimbaud Sébastien <sardren34@gmail.com>
2017-03-18 22:42:51 +0100

    Mise en fonction script Tuto

--- scripts/tutorial_pycom/LoraRAW.py
+++ scripts/tutorial_pycom/LoraRAW.py
@@ -5,20 +5,21 @@ import time
 
 # initialize LoRa in LORA mode
 # more params can also be given, like frequency, tx power and spreading factor
-lora = LoRa(mode=LoRa.LORA)
+def LoraRAW() :
+    lora = LoRa(mode=LoRa.LORA)
 
 # create a raw LoRa socket
-s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
-while True:
-    # send some data
-    s.setblocking(True)
-    s.send('Hello')
+    while True:
+        # send some data
+        s.setblocking(True)
+        s.send('Hello')
 
-    # get any data received...
-    s.setblocking(False)
-    data = s.recv(64)
-    print(data)
+        # get any data received...
+        s.setblocking(False)
+        data = s.recv(64)
+        print(data)
 
-    # wait a random amount of time
-    time.sleep(machine.rng() & 0x0F)
+        # wait a random amount of time
+        time.sleep(machine.rng() & 0x0F)
