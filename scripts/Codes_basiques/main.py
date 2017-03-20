e4ef0edf309d0400c798ffcc4f95a0e6fca59e73
louvetg <Gaspard.louvet@phelma.grenoble-inp.fr>
2017-03-21 00:07:53 +0100

    Protocole simple valable en v1.6.7.b1

---
+++ scripts/Codes_basiques/main.py
@@ -0,0 +1,25 @@
+# main.py -- put your code here!
+from network import LoRa
+import socket
+import machine
+import time
+
+# initialize LoRa in LORA mode
+# more params can also be given, like frequency, tx power and spreading factor
+lora = LoRa(mode=LoRa.LORA, frequency=863000000, power_mode=LoRa.ALWAYS_ON, tx_power=14, bandwidth=LoRa.BW_250KHZ,  sf=7,  preamble=8,  coding_rate=LoRa.CODING_4_5,  tx_iq=False,  rx_iq=False)
+
+# create a raw LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+while True:
+    # send some data
+    s.setblocking(True)
+    s.send('Hello-COM5')
+
+    # get any data received...
+    s.setblocking(False)
+    data = s.recv(64)
+    print(data)
+
+    # wait a random amount of time
+    time.sleep(machine.rng() & 0x0F)
